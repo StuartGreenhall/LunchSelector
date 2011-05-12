b612bda0dce240a144f4aa2c48e99da862620e61
Noppanit Charassinvichai <ncharass@thoughtworks.com>
2011-05-12 11:07:52 +0100

    [ToyC] Added prepared_data for refactoring

--- lunchSelector.rb
+++ lunchSelector.rb
@@ -124,6 +124,17 @@ get '/menu/excludedDishes/separatewithamp/:answer' do
   haml :excludedDishesBasedOnTwoAnswers
 end
 
+def prepares_data(hash_nodes)
+  @array_of_node = Array.new
+  hash_nodes.each do | node |
+    nodeId = node.values_at('self')[0].split('/').last
+    text = node.values_at('data')[0].values_at('name')
+    @array_of_node << { :nodeId => nodeId, :text => text } 
+  end
+  
+  return @array_of_node
+end
+
 
 
 
